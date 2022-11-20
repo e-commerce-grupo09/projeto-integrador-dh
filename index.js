159743fe5215694b7574e62aa4e2856f7fd2c40b
thiagoBiancareli <thiagob2mt@gmail.com>
2022-11-20 15:02:48 -0300

    Servidor criado

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Olá");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(__dirname +('public')));
+
+servidor.get('/produto',(req, res)=>{
+    res.sendFile(__dirname + '/views/produto.html')
+});
+
+servidor.get('/carrinho',(req, res)=>{
+    res.sendFile(__dirname + '/views/carrinho.html')
+});
+
+servidor.listen(3000);
