21cb574918c27d1ceae67f646ff9f54bbc9a4208
thiagoBiancareli <thiagob2mt@gmail.com>
2023-01-20 16:10:37 -0300

    Atualizando branch

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/',(req, res)=>{
+    return res.sendFile(__dirname + '/views/home.html')
+});
+
 app.get('/produtos',(req, res)=>{
     return res.sendFile(__dirname + '/views/produtos.html')
 });
@@ -13,13 +17,15 @@ app.get('/carrinho',(req, res)=>{
     return res.sendFile(__dirname + '/views/carrinho.html')
 });
 
-app.get('/home',(req, res)=>{
-    return res.sendFile(__dirname + '/views/home.html')
-});
-
 app.get('/login',(req, res)=>{
     return res.sendFile(__dirname + '/views/login.html')
 });
+
+app.get('/sac',(req, res)=>{
+    return res.sendFile(__dirname + '/views/sac.html')
+});
+
+
 
 
 app.listen(3000);
